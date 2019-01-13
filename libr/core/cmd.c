4339d039cb9720820778133527bc9b231b33e4a5
pancake <pancake@nopcode.org>
2019-01-13 12:49:41 +0100

    WIP: Intial support for rotozooming strings

--- libr/core/cmd.c
+++ libr/core/cmd.c
@@ -1,4 +1,4 @@
-/* radare - LGPL - Copyright 2009-2018 - nibble, pancake */
+/* radare - LGPL - Copyright 2009-2019 - nibble, pancake */
 #if 0
 * Use RList
 * Support callback for null command (why?)
